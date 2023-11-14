11a42b93195e95c5a208d51361a4d6c9ffa84ce1
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-14 16:34:21 +0100

    Entête

--- Project/Views/Homepage/entete.html.php
+++ Project/Views/Homepage/entete.html.php
@@ -3,7 +3,7 @@
 <head>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="entete.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Le meilleur des années 80</title>
     <nav class="main-navigation">
@@ -12,7 +12,7 @@
         </div>
         <ul class="nav-list">
             <li class="nav-list-item">
-                <a href="#" class="nav-link">Acceuil</a>
+                <a href="#" class="nav-link">Accueil</a>
             </li>
             <li class="nav-list-item">
                 <a href="#" class="nav-link">Ajouter Chanson</a>
