3b649168a6c03a1a38706293f317232e6e47a553
Nicomegaa <96539816+Nicomegaa@users.noreply.github.com>
2023-11-14 11:46:04 +0100

    Create Utilisateur.php

---
+++ Project/Classes/Utilisateur.php
@@ -0,0 +1,33 @@
+<?php
+
+class Utilisateur
+{
+    private string $nom;
+    private string $prenom;
+    private string $login;
+    private string $mdp;
+
+    public function __construct($nom,$prenom,$login,$mdp)
+    {
+        $this->nom=$nom;
+        $this->prenom=$prenom;
+        $this->login=$login;
+        $this->mdp=$mdp;
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function getLogin()
+    {
+        return $this->login;
+    }
+    public function getMdp
+    {
+        return $this->mdp;
+    }
+}
