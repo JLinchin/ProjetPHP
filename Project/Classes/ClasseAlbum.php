3657913539ef8bf1e727cd7bbe977279ca51755c
lpaulze <148073658+lpaulze@users.noreply.github.com>
2023-11-14 11:40:27 +0100

    création de la classe Album

---
+++ Project/Classes/ClasseAlbum.php
@@ -0,0 +1,29 @@
+<?php
+class Album
+{
+	private $id;
+    private $nom;
+    private $lienImage;
+
+    public function Personne($id, $nom,$lienImage) {
+	$this->id = $id;
+	$this->nom = $nom;
+    $this->lienImage = $lienImage;
+    }
+
+    public function __get($propriete) {
+	switch ($propriete) {
+		case 'id' : return $this->prenom; break;
+		case "nom" : return $this->nom; break;
+        case "lienImage" : return $this->lienImage; break;
+	    }
+    }
+    public function __set($propriete, $value) {
+	switch ($propriete) {
+		case "id" : $this->id = $value; break;
+		case "nom" : $this->nom = $value; break;
+        case "lienImage" : $this->lienImage = $value; break;
+	}
+}
+}
+?>
