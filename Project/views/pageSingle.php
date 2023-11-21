a0aebd38385855c15ff4f63d5f686d9de781d036
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-21 09:36:38 +0100

    Page Single

--- Project/views/pageSingle.php
+++ Project/views/pageSingle.php
@@ -3,18 +3,23 @@
 <html>
     <head>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <link rel="stylesheet" href="css/pageSingle.css">
+        <link rel="stylesheet" href="../css/pageSingle.css">
     </head>
 
     <body>
-        <div class="Single">
-            <h1 class="titreSingle">Titre du Single</h1>
-            <img class="imgAlbum" src="../../Projects/Assets/Jaquette.jpg"></img>
+        <div class="single">
+            <div class="infoSingle"></div>
+                <h1 class="titreSingle">Titre du Single</h1>
+                <img class="imgAlbum" src="../../Project/Assets/Jaquette.jpg"></img>
+                <h2 class="nomInterprete">Interprete</h2>
+                <h2 class="titreAlbum">Album</h2>
+                <h2 class="duree">00:00</h2>
+            </div>
         </div>
-        <div class="Paroles">
+        <div class="paroles">
 
         </div>
-        <div class="Button">
+        <div class="button">
             <button class="btnModif" type="button">Modifier</button>
             <button class="btnSupp" type="button">Supprimer</button>
         </div>
