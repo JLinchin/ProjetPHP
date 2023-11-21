d55d0602d84a01d24e89f590558b0aad4034d4cb
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-21 14:58:04 +0100

    Page single

--- Project/views/pageSingle.php
+++ Project/views/pageSingle.php
@@ -8,19 +8,25 @@
 
     <body>
         <div class="single">
-            <div class="infoSingle"></div>
+            <div class="infoSingle">
                 <h1 class="titreSingle">Titre du Single</h1>
                 <img class="imgAlbum" src="../../Project/Assets/Jaquette.jpg"></img>
                 <h2 class="nomInterprete">Interprete</h2>
                 <h2 class="titreAlbum">Album</h2>
                 <h2 class="duree">00:00</h2>
             </div>
-        </div>
-        <div class="paroles">
-
+            <div class="paroles">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+            </div>
         </div>
         <div class="button">
+            <div class="wrap">
             <button class="btnModif" type="button">Modifier</button>
             <button class="btnSupp" type="button">Supprimer</button>
+            </div>
         </div>
     </body>
