abe759d8c7b385b060332f56d61aca5dd69bebc2
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-21 17:11:26 +0100

    Page single

--- Project/views/pageSingle.php
+++ Project/views/pageSingle.php
@@ -4,13 +4,15 @@
     <head>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="../css/pageSingle.css">
+        <link rel="stylesheet" href="../css/entete.css">
     </head>
 
     <body>
+    <?php include('entete.php'); ?>
         <div class="single">
+        <h1 class="titreSingle">Titre du Single</h1>
             <div class="infoSingle">
-                <h1 class="titreSingle">Titre du Single</h1>
-                <img class="imgAlbum" src="../../Project/Assets/Jaquette.jpg"></img>
+                <img class="imgAlbum" src="../Images/22.BackToFront.jpg"></img>
                 <div class="infoDuSingle">
                     <h2 class="nomInterprete">Interprete</h2>
                     <h2 class="titreAlbum">Album</h2>
@@ -18,33 +20,7 @@
                 </div>
             </div>
             <div class="paroles">
-                    <p>
-                    Y a des traficante à l'étage
-                    Cet été, c'est cocktail, voyage, plage
-                    J'mets le compte à rebours, c'est un, deux, trois (Shady)
-                    Y a des traficante à l'étage
-                    Cet été, c'est cocktail, voyage, plage
-                    J'mets le compte à rebours, c'est un, deux, trois
-                    Elle m'appelle mon amour, j'sors le KX dans la cité
-                    À midi ça ouvre le four, à 16 heures ça se fait relayer
-                    J'sors de GAV, les kham ont gardé mes lacets
-                    Y a son petit boule dans la cuisine, le 'zin t'abusais sur la C
-                    Tu m'critiques, je m'en bats les les
-                    J'suis arrivé là en indé
-                    À Marseille que des cala, ça s'canne sur flash et baile
-                    Et sur PP, cette année faut tout niquer
-                    Elle veut s'asseoir sur la fusée, zinc, j'suis broliqué
-                    Y a trop de bâtards, on sait tout
-                    Ça fait des manières, ça rend fou
-                    Ça m'dit "t'inquiètes, ouais y a la roue"
-                    Beh, nique ta, toi et la roue
-                    C'est TT, encore on nique tout
-                    On a le cocktail des voyous
-                    Elle est posée dans le RS
-                    Sur PP, son boule, il nique tout
-                    Y a des traficantes à l'étage
-                    Cet été, c'est cocktail, voyage, plage
-                    J'mets le compte à rebours, c'est un, deux, trois
+                    <p>La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
@@ -53,25 +29,6 @@
                     La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
-                    La gadji, c'est un paqueta
-                    Elle veut s'caler dans le Benz, dans le GLA
-                    Elle aime bien les ninjas
-                    Elle a mis ma veste Yamaha
-                    Il veut toucher l'mousseux pour aller voir un paqueta
-                    On va lever nos verres, à la ceinture un Beretta
-                    J'fume un gros pét' dans l'pilotis
-                    Dans l'appart' ça coupe le shit
-                    Ça roule sans permis en Audi
-                    Viens me faire décaler, ma petite (décaler, ma petite)
-                    Viens me faire décaler, ma petite
-                    Parce que dans le quartier les poussettes, oui ça va vite
-                    Ça se cale sous flash et pétou
-                    Ça baraude l'été, ça coupe un 30 de bambous (ouh ouh)
-                    Et le secteur est miné, y a des schmitts partout
-                    Faut lever tout, leur faire des coucous (ouh ouh)
-                    Y a des traficante à l'étage
-                    Cet été, c'est cocktail, voyage, plage
-                    J'mets le compte à rebours, c'est un, deux, trois
                     La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
                     La gadji, c'est un paqueta
