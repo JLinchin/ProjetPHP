53a46fb315bc08abc35f5d947afbcb2a78f3b14d
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-21 09:02:23 +0100

    Page single

--- Project/views/pageSingle.php
+++ Project/views/pageSingle.php
@@ -3,11 +3,13 @@
 <html>
     <head>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link rel="stylesheet" href="css/pageSingle.css">
     </head>
 
     <body>
         <div class="Single">
-
+            <h1 class="titreSingle">Titre du Single</h1>
+            <img class="imgAlbum" src="../../Projects/Assets/Jaquette.jpg"></img>
         </div>
         <div class="Paroles">
 
