ec234c3e327d0e4d01e5914ba3b94cd3e0a094f0
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-21 08:54:08 +0100

    Page Single

---
+++ Project/views/pageSingle.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    </head>
+
+    <body>
+        <div class="Single">
+
+        </div>
+        <div class="Paroles">
+
+        </div>
+        <div class="Button">
+            <button class="btnModif" type="button">Modifier</button>
+            <button class="btnSupp" type="button">Supprimer</button>
+        </div>
+    </body>
