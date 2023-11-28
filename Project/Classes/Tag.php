0d666eb7c2fde4da2b996b714502e4fbd6579269
lpaulze <148073658+lpaulze@users.noreply.github.com>
2023-11-28 11:22:07 +0100

    Ajout du model TAG et Tagger

--- Project/Classes/Tag.php
+++ Project/Classes/Tag.php
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
 
-<?php
-class Album
-{
-	private $id;
-    private $libelle;
-
-    public function Personne($id, $libelle) {
-	$this->id = $id;
-	$this->libelle = $libelle;
-    }
-
-    public function __get($propriete) {
-	switch ($propriete) {
-		case 'id' : return $this->prenom; break;
-		case "libelle" : return $this->libelle; break;
-	    }
-    }
-    public function __set($propriete, $value) {
-	switch ($propriete) {
-		case "id" : $this->id = $value; break;
-		case "libelle" : $this->libelle = $value; break;
-	}
-}
-}
-=======
 <?php
 
 class Tag
@@ -60,6 +34,4 @@ class Tag
         }
     }
 }
-
->>>>>>> 95aa1662652ba3a1a9abd272a405f5b4b4f4bf11
 ?>
---
+++ Project/Models/bd.Tag_Tagger.php
@@ -0,0 +1,145 @@
+<?php
+
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__)
+    $racine = "..";
+
+include_once "bd.inc.php";
+include_once "$racine/classes/Tag.php";
+include_once "$racine/classes/Tagger.php";
+
+function getTags()
+{
+    $lesChnasons = array();
+    try
+    {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from tag");
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        while ($ligne)
+        {
+            $unTag = new Chanson($ligne["id"], $ligne["libelle"]);
+            $lesTags[] = $unTag;
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur ! :" . $e->getMessage();
+        die();
+    }
+
+    return $lesTags;
+}
+
+
+function getTagByIdC($idT)
+{
+    try
+    {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from tag where id = :idT");
+        $req = bindValue(':idT', $idT, PDO::PARAM_INT);
+
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        $unTag = new Chanson($ligne["id"], $ligne["libelle"]);
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+
+    return $unTag;
+}
+
+
+function getTagByChanson($uneChanson)
+{
+    try
+    {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from Tagger inner join Tag on  idTag = id where idChanson = :uneChanson");
+        $req = bindValue(':uneChanson', $uneChanson, PDO::PARAM_INT);
+
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        $unTag = new Chanson($ligne["id"], $ligne["libelle"]);
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+
+    return $unTag;
+}
+
+function AddTag($unTag,$uneChanson)
+{
+    try
+    {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("insert into Tagger (idTag, idChanson) Values(:unTag, :uneChanson");
+        $req = bindValue(':unTag', $unTag, PDO::PARAM_INT);
+        $req = bindValue(':uneChanson', $uneChanson, PDO::PARAM_INT);
+
+        $req->execute();
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+}
+
+function MajTag($ancienTag,$nouveauTag,$uneChanson)
+{
+    try
+    {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("Update Tagger idTag = :nouveauTag, idChanson = :uneChanson where idTag = :ancienTag");
+        $req = bindValue(':nouveauTag', $nouveauTag, PDO::PARAM_INT);
+        $req = bindValue(':ancienTag', $ancienTag, PDO::PARAM_INT);
+        $req = bindValue(':uneChanson', $uneChanson, PDO::PARAM_INT);
+
+        $req->execute();
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+}
+
+function SupprimerTag($unTag,$uneChanson)
+{
+    try
+    {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("Delete From Tagger Where idTag = :unTag and idChanson = :uneChanson");
+        $req = bindValue(':unTag', $unTag, PDO::PARAM_INT);
+        $req = bindValue(':uneChanson', $uneChanson, PDO::PARAM_INT);
+
+        $req->execute();
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+}
+
+?>
+
+
