73e4471ca850af5fb460fdd346e69c0f4479d4af
Dev B <joseph.minchin@gmail.com>
2023-11-28 10:43:21 +0100

    Modification de la structure du projet (création d'un dossier pour les scripts JavaScript et séparation des fichiers css et js de bootstrap)

--- Project/views/ajouterChanson.php
+++ Project/views/ajouterChanson.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<link href="./css/bootstrap/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="./js/bootstrap/bootstrap.min.js"></script>
 <link rel="stylesheet" href="../css/ajouterChanson.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <body>
--- Project/views/pageSingle.php
+++ Project/views/pageSingle.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link href="./css/bootstrap/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel="stylesheet" href="../css/pageSingle.css">
         <link rel="stylesheet" href="../css/entete.css">
     </head>
