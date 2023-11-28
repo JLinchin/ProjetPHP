22bdfbe8ad4ff23d9f2cf0f181e401f9f6b205e3
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-11-28 11:14:33 +0100

    Modèle classe Album

--- Project/Models/bd.album.inc.php
+++ Project/Models/bd.album.inc.php
@@ -31,11 +31,72 @@ public function getAlbum(){
     return $lesAlbums;
 }
 
+public function getAlbumById($idA){
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from album where id = :idA");
+        $req = bindValue(':idA', $idA, PDO::PARAM_INT);
+
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        $unAlbum = new Chanson($ligne["id"], $ligne["nom"], $ligne["lienImage"]);
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $unAlbum;
+}
+
+public function getAlbumByTitre($titre){
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from album where nom = :titre");
+        $req = bindValue(':titre', $titre, PDO::PARAM_INT);
+
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        $unAlbum = new Chanson($ligne["id"], $ligne["nom"], $ligne["lienImage"]);
+    }
+
+    catch (PDOException $e)
+    {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $unAlbum;
+}
+
 public function getAlbumByIdChanson($idChanson){
     try{
         $cnx = connexionPDO();
         $req = $cnx->prepare("select * from album inner join chanson on id = idAlbum where id = :idChanson");
         $req = bindValue(':idChanson', $idChanson, PDO::PARAM_INT);
+
+        $req->execute();
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        $unAlbum = new Chanson($ligne["id"], $ligne["nom"], $ligne["lienImage"]);
+    }
+    catch (PDOException $e)
+    {
+        print "Erreur ! :" . $e->getMessage();
+        die();
+    }
+    return $unAlbum;
+}
+
+public function getAlbumByTitreChanson($titreC){
+    try{
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("SELECT Album.*
+                                FROM Album
+                                JOIN Chanson ON Album.id = Chanson.idAlbum
+                                WHERE Chanson.nom = :titre");
+        $req = bindValue(':titreC', $titreC, PDO::PARAM_INT);
 
         $req->execute();
         $ligne = $req->fetch(PDO::FETCH_ASSOC);
