675df5f7d7002e19656b48976c57543e1f805bbd
Dev B <joseph.minchin@gmail.com>
2023-11-28 14:42:08 +0100

    Modification des fichiers de vue

--- Project/views/ajouterChanson.php
+++ Project/views/ajouterChanson.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<link href="./css/bootstrap/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="./js/bootstrap/bootstrap.min.js"></script>
+<link href="../css/bootstrap/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="../js/bootstrap/bootstrap.min.js"></script>
 <link rel="stylesheet" href="../css/ajouterChanson.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <body>
--- Project/views/enpied.php
+++ Project/views/enpied.php
@@ -1,4 +1,4 @@
     </body>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="./js/bootstrap/bootstrap.min.js"></script>
+    <script src="../js/bootstrap/bootstrap.min.js"></script>
 </html>
