fd8b8dd1c350f2825f3a86b92bc848a587a53eb1
Nicomegaa <96539816+Nicomegaa@users.noreply.github.com>
2023-12-05 10:22:13 +0100

    Contrôleur de recherche fonctionnel

--- Project/views/recherche.php
+++ Project/views/recherche.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="../css/recherche.css">
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recherche</title>
+    <link rel="stylesheet" href="../css/recherche.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!--<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <!--<script src="../js/bootstrap/bootstrap.min.js"></script>-->
+    <script type="text/javascript" src="../js/scriptsR.js"></script>
+</head>
 <body>
+
+
     <!--Barre de recherche-->
     <div class="container h-100">
         <div class="d-flex justify-content-center h-25">
           <div class="searchbar">
-            <input class="search_input" type="text" name="" placeholder="Recherche">
+            <input class="search_input" type="text" name="" placeholder="Recherche" id="zoneRecherche">
             <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+            <div id="display"></div>
           </div>
         </div>
       </div>
 </body>
+</html>
