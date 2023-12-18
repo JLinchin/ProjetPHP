3f080b6f88c2b92aa70a86eb8d46a9778d4d7bf9
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2023-12-18 17:24:02 +0100

    Début du footer

--- Project/views/enpied.php
+++ Project/views/enpied.php
@@ -1,4 +1,10 @@
     </body>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="../js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+
+    <footer>
+        <div class="container">
+            
+        </div>
+    </footer>
 </html>
