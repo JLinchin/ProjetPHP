64a8e6b8a5cecd9af42f02a9694001f60dbbf6a3
Joseph Minchin <joseph.minchin@gmail.com>
2024-03-03 22:34:37 +0100

    Modification de l'affichage des mots de passe

--- Project/views/inscription.php
+++ Project/views/inscription.php
@@ -3,7 +3,7 @@
         <input class="Ajout" type="text" name="prenom" placeholder="Prenom">
         <input class="Ajout" type="text" name="nom" placeholder="Nom">
         <input class="Ajout" type="text" name="login" placeholder="Login">
-        <input class="Ajout" type="text" name="mdp" placeholder="Mot de passe">
+        <input class="Ajout" type="password" name="mdp" placeholder="Mot de passe">
         <button class="connexion" type="submit">Inscription</button>
    </form>
 </div>
