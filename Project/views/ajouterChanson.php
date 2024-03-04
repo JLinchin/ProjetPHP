d6ac8a0d2d64f217a4ba63102748693cae08f9fc
Paul Fernandes <88905397+63Paulo@users.noreply.github.com>
2024-03-04 12:28:59 +0100

    Ajouter chanson

--- Project/views/ajouterChanson.php
+++ Project/views/ajouterChanson.php
@@ -1,4 +1,3 @@
-
 <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="js/bootstrap/bootstrap.min.js"></script>
 <link rel="stylesheet" href="css/ajouterChanson.css">
@@ -6,18 +5,21 @@
 <body>
           
           <div class="insertion">
-            <form action="">
+            <form action="" methode="POST">
               <input class="Ajout" type="text" name="Interprete" placeholder="Interpréte">
               <input class="Ajout" type="text" name="Single" placeholder="Titre Single">
               <input class="Ajout" type="text" name="Duree" placeholder="Durée">
               <input class="Ajout" type="text" name="DateSortie" placeholder="DateSortie">
               <input class="Ajout" type="text" name="Genre" placeholder="Genre">
-              <input class="Ajout" type="text" name="MeilleurePlace" placeholder="Meilleure place">
+              <input class="Ajout" type="text" name="MeilleurePlace" placeholder="MeilleurePlace">
               <input class="Ajout" type="text" name="Parole" placeholder="Parole">
-              <input class="Ajout" type="select" name="Album" placeholder="Album">
+              <select class="Ajout" name="Album" placeholder="Album">
+              <?php foreach($lesAlbums as $unAlbum){
+              echo "<option value=" . $unAlbum->id . ">". $unAlbum->nom;
+              }
+              ?> </option>
+              </select>
               <button class="valider" type="button">Valider</button>
             </form>
           </div>
-
-          <script src = "js/ajouterChanson.js"></script>
 </body>
